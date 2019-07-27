f46a6649851462c75492fa29ea38a8cbfdf68f2e
Akihiro Yamada <u.parr917@gmail.com>
2019-07-27 23:15:28 +0900

    分、時間のタイマーを実装。

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -15,12 +15,14 @@ function startCount(): void {
   if (workStartBtn != null) {
     let numClicked = 0;
     let count = 0;
+    let minCount = 0;
+    let hourCount = 0;
     let timer: any;
     workStartBtn.addEventListener('click', () => {
       numClicked++;
       if (workStartBtn != null) {
         if (numClicked % 2 == 1) {
-          workStartBtn.innerHTML = '停止';
+          workStartBtn.innerHTML = '一時停止';
           workStartBtn.classList.add('btn-danger');
           workStartBtn.classList.remove('btn-primary');
         } else {
@@ -30,11 +32,28 @@ function startCount(): void {
         }
 
         let secElm = document.getElementById('work-sec');
+        let minElm = document.getElementById('work-min');
+        let hourElm = document.getElementById('work-hour');
         if (numClicked % 2 != 0) {
           timer = setInterval(() => {
             count++;
             if (secElm != null) {
+              if (count == 60) {
+                count = 0;
+                minCount++;
+                if (minCount == 60) {
+                  minCount = 0;
+                  hourCount++;
+                }
+                if (minElm != null) {
+                  minElm.innerHTML = ('0' + String(minCount)).slice(-2);
+                }
+                if (hourElm != null) {
+                  hourElm.innerHTML = ('0' + String(hourCount)).slice(-2);
+                }
+              }
               secElm.innerHTML = ('0' + String(count)).slice(-2);
+
             }
           }, 1000);
         } else {
