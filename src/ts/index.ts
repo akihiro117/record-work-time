eadd604a6b6ac4c18c58eecae4a2a03a8db21887
Akihiro Yamada <u.parr917@gmail.com>
2019-07-27 22:10:41 +0900

    秒をカウントアップする処理を実装。

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,11 +4,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // BootstrapのJavaScript側の機能を読み込む
 import 'bootstrap';
 
-import { sample } from './operate-time';
+// Htmlを読み込んだ後に実行。
+document.addEventListener('DOMContentLoaded', () => {
+  startCount();
+}, false);
 
-let workStartBtn = document.getElementById('work-name-btn');
-if (workStartBtn != null) {
-  workStartBtn.addEventListener('click', () => {
-    alert("test");
-  }, false);
-}
+function startCount(): void {
+  let workStartBtn = document.getElementById('work-start-btn');
+  if (workStartBtn != null) {
+    workStartBtn.addEventListener('click', () => {
+      if (workStartBtn != null) {
+        workStartBtn.innerHTML = '停止';
+        workStartBtn.classList.add('btn-danger');
+        workStartBtn.classList.remove('btn-primary');
+      }
+      countSec();
+    });
+  }
+};
+
+function countSec(): void {
+  let secElm = document.getElementById('work-sec');
+  let count = 0;
+  let timer = setInterval(() => {
+    count++;
+    if (secElm != null) {
+      secElm.innerHTML = ('0' + String(count)).slice(-2);
+    }
+  }, 1000);
+};
