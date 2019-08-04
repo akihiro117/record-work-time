377ff5a7506258d4b73babc5eb9b1662b4fe64d2
Akihiro Yamada <u.parr917@gmail.com>
2019-08-05 00:09:14 +0900

    jqueryを使ってcloneするやり方に変更。

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // BootstrapのJavaScript側の機能を読み込む
 import 'bootstrap';
 
+import $ from 'jquery';
 
 // Htmlを読み込んだ後に実行。
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,24 +27,30 @@ function addStopwatch(): void {
   // 作業追加ボタン。
   let addWorkBtn = document.getElementById('work-add-btn');
   if (addWorkBtn != null) {
-    // 現在表示されている作業の数。
+    // 現在表示されている作業の数。複製した要素のIDに付与する。
     let numWork = 1;
     addWorkBtn.addEventListener('click', () => {
+      let targetElm = $('#' + STOPWATCH_ID_PREFIX + numWork);
+      numWork++;
+      // stopwatchをを複製。
+      targetElm.clone().insertAfter(targetElm).attr('id', STOPWATCH_ID_PREFIX + numWork);
 
-      // stopwatchをクローンする。
-      let stopwatchElm = document.getElementById('stopwatch1');
-      if (stopwatchElm != null) {
-        // stopwatchのクローンを生成。
-        let cloneStopwatch = stopwatchElm.cloneNode(true);
 
-        // cloneして生成した要素のvalueを初期化。
-        let cloneChildList = cloneStopwatch.childNodes[1];
+      // // stopwatchをクローンする。
+      // let stopwatchElm = document.getElementById('stopwatch1');
+      // if (stopwatchElm != null) {
+      //   // stopwatchのクローンを生成。
 
-        let stopwatchesElm = document.getElementById('stopwatches');
-        if (stopwatchesElm != null) {
-          stopwatchesElm.appendChild(cloneStopwatch);
-        }
-      }
+      //   let cloneStopwatch = stopwatchElm.cloneNode(true);
+
+      //   // cloneして生成した要素のvalueを初期化。
+      //   let cloneChildList = cloneStopwatch.childNodes[1];
+
+      //   let stopwatchesElm = document.getElementById('stopwatches');
+      //   if (stopwatchesElm != null) {
+      //     stopwatchesElm.appendChild(cloneStopwatch);
+      //   }
+      // }
     });
   }
 
