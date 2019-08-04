4f561b1f58ca45276f6c30b84cf76f2e30942423
Akihiro Yamada <u.parr917@gmail.com>
2019-08-04 14:15:54 +0900

    stopwatchの要素をcloneする処理を実装。

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -17,7 +17,29 @@ document.addEventListener('DOMContentLoaded', () => {
   let stopwatch = new Stopwatch();
   // ストップウォッチ処理を行う。
   stopwatch.startTimerAfterClicking();
+
+  addStopwatch(1);
 }, false);
 
 
+function addStopwatch(numStopwatch: number): void {
+  const STOPWATCH_ID_PREFIX = 'stopwatch';
+  // 作業追加ボタン。
+  let addWorkBtn = document.getElementById('work-add-btn');
+  if (addWorkBtn != null) {
+    let numWork = 1;
+    addWorkBtn.addEventListener('click', () => {
 
+      // stopwatchをクローンする。
+      let stopwatchElm = document.getElementById('stopwatch1');
+      if (stopwatchElm != null) {
+        let cloneStopwatch = stopwatchElm.cloneNode(true);
+        let stopwatchesElm = document.getElementById('stopwatches');
+        if (stopwatchesElm != null) {
+          stopwatchesElm.appendChild(cloneStopwatch);
+        }
+      }
+    });
+  }
+
+}
